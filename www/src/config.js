1498edc3ba48c4910fe0841965848d4feb9b0aa5
wTheRockb <forrestwbutler@gmail.com>
2020-11-23 13:26:01 -0500

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "23k8f3lsestshn35klokok8gbn",     // CognitoClientID
-  "api_base_url": "https://97m2qtdlf5.execute-api.us-west-2.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "api_base_url": "https://97m2qtdlf5.execute-api.us-west-2.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-woody-serverless.auth.us-west-2.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d1uby1me92zf6a.amplifyapp.com"                                      // AmplifyURL
 };
